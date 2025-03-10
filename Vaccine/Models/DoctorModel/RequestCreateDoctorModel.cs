d22c34b42eeb24464bc10db3368e014561731aed
zollie <thaonguyen07112004@gmail.com>
2025-03-10 23:45:35 +0700

    add doctor controller

---
+++ Vaccine/Models/DoctorModel/RequestCreateDoctorModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Vaccine.API.Models.DoctorModel
+{
+    public class RequestCreateDoctorModel
+    {
+        public string Name { get; set; } = null!;
+        public int? Age { get; set; }
+        public string? Gender { get; set; }
+        public string Phone { get; set; } = null!;
+        public string? Address { get; set; }
+        public string? Degree { get; set; }
+        public int? ExperienceYears { get; set; }
+        public DateOnly Dob { get; set; }
+    }
+}
